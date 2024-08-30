9861e723afc049d35cd6ea59164bfeab4eb7286a
SchneiderSeba <seba_19_sc@hotmail.com>
2024-08-30 18:00:08 +0200

    working

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 import express from 'express'
+import jwt from 'jsonwebtoken'
+import cookieParser from 'cookie-parser'
 import { PORT, JWT_SECRET_KEY } from './config.js'
 import { UserRepository } from './user-repository.js'
-import jwt from 'jsonwebtoken'
 
 const app = express()
-app.set('view engine', 'ejs')
 app.use(express.json())
+app.use(cookieParser())
+app.use((req, res, next) => {
+  const token = req.cookies.access_token
+  req.session = { user: null }
+
+  try {
+    const data = jwt.verify(token, JWT_SECRET_KEY)
+    req.session.user = data
+  } catch (error) {}
+  next()
+})
+app.set('view engine', 'ejs')
 
 app.get('/home', (req, res) => {
   res.sendFile(process.cwd() + '/index.html')
 })
 
 app.get('/', (req, res) => {
-  res.render('main')
+  const { user } = req.session
+  res.render('main', user)
 })
 app.post('/login', async (req, res) => {
   const { username, password } = req.body
@@ -21,7 +34,8 @@ app.post('/login', async (req, res) => {
   try {
     const user = await UserRepository.login({ username, password })
     const token = jwt.sign({ id: user._id, username: user.username }, JWT_SECRET_KEY, { expiresIn: '1h' })
-    res.send({ user })
+    res.cookie('access_token', token, { httpOnly: true, maxAge: 1000 * 60 * 60 })
+      .send({ user, token })
   } catch (error) {
     res.status(401).send(error.message)
   }
@@ -37,10 +51,16 @@ app.post('/register', async (req, res) => {
     res.status(400).send(error.message)
   }
 })
-app.post('/logout', (req, res) => {})
+app.post('/logout', (req, res) => {
+  res.clearCookie('access_token').json({ message: 'logout' })
+})
 
 app.get('/protected', (req, res) => {
-  res.render('protected', { user: 'ImperiumAO' })
+  const { user } = req.session
+  if (!user) {
+    return res.status(403).send.apply('Access not authorized').redirect('/')
+  }
+  res.render('protected', user)
 })
 
 app.listen(PORT, () => {
